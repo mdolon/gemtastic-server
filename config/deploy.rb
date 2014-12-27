bd586c8a67f4b280b51c2030319c47dc64da1535
Monji Dolon <mdolon@gmail.com>
2014-12-27 18:03:02 -0500

    Remove sudo requirement

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :repo_url, 'git@github.com:mdolon/gemtastic-server.git'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/www/gematastic.org/'
+set :use_sudo, false
 
 # Default value for :scm is :git
 # set :scm, :git
