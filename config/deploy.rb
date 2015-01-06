c8f0335a99ff2c2e5d4f7f8256d4de5eb31ff8d3
Monji Dolon <mdolon@gmail.com>
2015-01-06 14:05:35 -0500

    Update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,17 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'gemtastic-server'
+set :repo_url, 'git@github.com:mdolon/gemtastic-server.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, '/home/www/gematastic.org/'
+set :ssh_options, {
+  forward_agent: true
+}
 
 # Default value for :scm is :git
 # set :scm, :git
