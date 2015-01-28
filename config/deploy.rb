8cd156fc55d0761bdf62f0c6cab572ce0162f192
Monji Dolon <mdolon@gmail.com>
2015-01-28 11:03:58 -0500

    Move back and rename

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,3 +99,11 @@ namespace :deploy do
   # automatically.
   after 'deploy:publishing', 'deploy:restart'
 end
+
+namespace :raker do
+  desc "Run a task on a remote server."
+  # run like: cap staging raker:invoke task=a_certain_task
+  task :invoke do
+    run("cd #{deploy_to}/current; /usr/bin/env bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")
+  end
+end
