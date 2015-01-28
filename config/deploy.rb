d07e237f5ac8eafd76a9c12a7ce7e00a1914cd48
Monji Dolon <mdolon@gmail.com>
2015-01-28 10:42:33 -0500

    Add rake task to update and fetch remotely from local box

--- config/deploy.rb
+++ config/deploy.rb
@@ -99,3 +99,11 @@ namespace :deploy do
   # automatically.
   after 'deploy:publishing', 'deploy:restart'
 end
+
+namespace :rake do  
+  desc "Run a task on a remote server."  
+  # run like: cap staging rake:invoke task=a_certain_task  
+  task :invoke do  
+    run("cd #{deploy_to}/current; /usr/bin/env bundle exec rake #{ENV['task']} RAILS_ENV=#{rails_env}")  
+  end  
+end
